dcd510e68ccb59777a402a5f0df932605d70111f
Owl-media19 <alaohabeebolaleye@gmail.com>
2023-02-18 03:13:03 -0800

    Task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A program that prints with the funtion printf
+ * Return: 0 (success)
+ */
+int main(void)
+{
+printf("%d \n with proper grammar, but the outcome is a piece of art",);
+return (0)
+}
