97205ed04d69ad01c05e65692f02510b0b363679
Owl-media19 <alaohabeebolaleye@gmail.com>
2023-02-18 02:56:50 -0800

    Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - a c progam that prints the same value
+ *
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return(0)
+}
