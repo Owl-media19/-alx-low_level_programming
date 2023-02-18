3e8d862dcaed28699b3b29aff40278ec57e83fa2
Owl-media19 <alaohabeebolaleye@gmail.com>
2023-02-18 05:17:47 -0800

    TAsk 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <unistd.d>
+#include <unistd.h>
 /**
  * main - A program that prints a line to the standard error
  * Return 1 (success)
@@ -7,5 +7,5 @@
 int main(void)
 {
 	fput("and that place of art is useful\" - Dora Korpar,2015-10-19\n", stdout);
-	return (1);
+return (1);
 }
