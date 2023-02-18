dd178a119400af839db7f30efe00267992097d16
Owl-media19 <alaohabeebolaleye@gmail.com>
2023-02-18 03:07:40 -0800

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main - a c progam that prints the same value
+ * main - A c progam that prints the same value with put function
  * Return: 0 (success)
  */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
-return(0)
+return (0);
 }
