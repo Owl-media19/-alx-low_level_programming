e5635c3deaa58fc1e20666290a84158127470be5
Owl-media19 <alaohabeebolaleye@gmail.com>
2023-02-19 06:45:58 -0800

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - main function to print alphabets in lowercase
+ *
+ * Return: Always 0 (success)
+ *
+ */
+int main(void)
+{
+	int ch = 'a';
+	
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+	putchar ('\n');
+	return (0);
+}
+
