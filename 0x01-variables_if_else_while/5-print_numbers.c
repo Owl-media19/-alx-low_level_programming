a4ef3eaecc10e63053fa29153c3252097af9a753
Owl-media19 <alaohabeebolaleye@gmail.com>
2023-02-20 03:33:48 -0800

    Task 6

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - to print numbers
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 52; i++)
+
+		putchar(i);
+		i++;
+		putchar('\n');
+	return (0);
+}
