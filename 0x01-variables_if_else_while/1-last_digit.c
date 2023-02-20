75e8d1d1c437bb346aad5b388aa0a9fff5798be9
Owl-media19 <alaohabeebolaleye@gmail.com>
2023-02-20 01:03:03 -0800

    TAsk 2

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -7,7 +7,6 @@
  * Return: Always 0 (success)
  *
  */
-
 int main(void)
 {
 	int n;
@@ -17,17 +16,17 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	printf("Last digit of %d is", n);
 	num = n % 10;
-	if (num > 5);
+	if (num > 5)
 	{
-	printf("%d and is greater than 5\n", num);
+		printf("%d and is greater than 5\n", num);
 	}
 	else if ((num < 6) && (num < 0));
 	{
-	printf("%d and is than 6 and not 0\n", num);
+		printf("%d and is than 6 and not 0\n", num);
 	}
 	else
 	{
-	printf("%d and is 0\n", num);
+		printf("%d and is 0\n", num);
 	}
-return (0);
+	return (0);
 }
--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <time.h>
 /**
- * main - main function to print alphabets in lowercase
+ * main -main function to print alphabets in lowercase
  *
  * Return: Always 0 (success)
  *
@@ -10,7 +10,7 @@
 int main(void)
 {
 	int ch = 'a';
-	
+
 	while (ch <= 'z')
 	{
 		putchar(ch);
