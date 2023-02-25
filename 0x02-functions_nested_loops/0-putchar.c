aedbd907fcbe7253d780d02d679af29af71b1b11
Owl-media19 <alaohabeebolaleye@gmail.com>
2023-02-24 22:02:44 -0800

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,13 +7,13 @@
 int main(void)
 {
 	int str[] = {95, 112, 117, 116, 99, 104, 97, 114};
-	int count, sz;
+	int count = 0, size;
 
-	sz = sizeof(str) / sizeof(int);
-	for (count = 0; count < sz; count++)
-	{
+	size = sizeof(str) / sizeof(int);
+	do{
 		_putchar(str[count]);
-	}
+		count++;
+	}while (count < size);
 	_putchar('\n');
 	return (0);
 }
