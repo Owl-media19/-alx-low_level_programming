4a1ae056e9314f149d45f44e6a920e3b62c51459
Owl-media19 <alaohabeebolaleye@gmail.com>
2023-03-21 15:10:56 -0700

    Object like macro

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,6 @@
+#ifndef _SUM_LIKE_MACRO
+#define _SUM_LIKE_MACRO
+
+#define SIZE 1024
+
+#endif 
---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,6 @@
+#ifndef _ABS_LIKE_MACRO
+#define _ABS_LIKE_MACRO
+
+#define ABS(x) ((x) < (0) ? ((x) * (-1)) : (x))
+
+#endif
